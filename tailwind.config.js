31760bfe11476b78218332b34409f7f1007e9126
Nanahawaw <folarinyetunde@gmail.com>
2024-01-29 23:33:33 +0100

    fixed header component issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust file extensions as needed
+  ],
   theme: {
     extend: {},
   },
