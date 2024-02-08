9bbad201fbe14081104325e9e9c1105e76ca95b9
Nanahawaw <folarinyetunde@gmail.com>
2024-02-08 20:34:27 +0100

    create signin api route

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/user.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +10,29 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("user created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) return next(errorHandler(404, "User not found "));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, "Incorrect Username or Password"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.jwtSecret);
+    const { password: pass, ...userInfo } = validUser._doc;
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(userInfo);
   } catch (error) {
     next(error);
   }
